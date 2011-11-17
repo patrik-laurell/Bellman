371b8758a7d26d85f6cd6cbac510f311d13037ba
Patrik Greco <sikevux@sikevux.se>
2011-11-18 00:16:22 +0100

    Fixed name of field in the table

--- setup.php
+++ setup.php
@@ -4,7 +4,7 @@ require_once "conn.php";
 $sql = <<<EOS
 CREATE TABLE IF NOT EXISTS users(
 	id int(11) NOT NULL auto_increment,
-	users varchar(50) NOT NULL default '',
+	name varchar(50) NOT NULL default '',
 	points int(11) NOT NULL default '0',
 	PRIMARY KEY(id)
 	)
